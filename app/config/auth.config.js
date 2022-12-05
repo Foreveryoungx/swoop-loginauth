8b7743bf6509a20ee3da9be520783e8bece26d00
Trey Allen <trevon.allen1994@gmail.com>
2022-12-04 22:28:11 -0600

    Server Project

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "tda-secret-key"
+}
---
+++ server.js
@@ -0,0 +1,90 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+
+const dbConfig = require("./app/config/db.config");
+
+const app = express();
+
+const corsOptions = {
+    origin : "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+
+//parse request
+app.use(express.json());
+
+//parse request of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieSession({
+    name : "session",
+    secret: "COOKIE_SECRET",
+    httpOnly: true
+})
+);
+
+/********** DataBase Connection **********/
+const db = require("./app/models")
+const Role = db.role;
+
+db.mongoose
+    .connect(`mongodb+srv://${dbConfig.username}:${dbConfig.password}@cluster0.sgtwfyw.mongodb.net/SwoopUser?retryWrites=true&w=majority`, {
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connect to MongoDB");
+    initial();
+}).catch ((err) => {
+    console.error("Connection error", err);
+    process.exit();
+});
+
+/********** Routes **********/
+app.get("/", (req, res) => {
+    res.json({ message: "Swoop API application." });
+});
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
+/********** Listen for Request on Port **********/
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
+/********** FN's **********/
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if(!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if(err) {
+                    console.log("error", err);
+                }
+                console.log("added 'user' to roles collection");
+            });
+
+            new Role({
+                name: "driver"
+            }).save(err => {
+                if(err) {
+                    console.log("error", err);
+                }
+                console.log("added 'driver' to roles collection");
+            });
+
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if(err) {
+                    console.log("error", err);
+                }
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
