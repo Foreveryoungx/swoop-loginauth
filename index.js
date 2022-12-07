2b6d94f3d33c64583775e4d25998047f8f7e50fa
Trey Allen <trevon.allen1994@gmail.com>
2022-12-06 23:30:46 -0600

    Update cors error to *

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.use(morgan("dev"));
 
 app.use(cors({
     origin: "*", // allow to server to accept request from different origin
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true // allow session cookie from browser to pass through
 }));
 //parse request
 
